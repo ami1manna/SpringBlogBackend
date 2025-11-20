9abff6a0682bdd198654183aa695d61060736fba
ami1manna <amitmannasm@gmail.com>
2025-11-20 19:23:18 +0530

    Added CommentController

---
+++ src/main/java/com/example/blog_api/controller/CommentController.java
@@ -0,0 +1,83 @@
+package com.example.blog_api.controller;
+
+import com.example.blog_api.dto.ApiResponse;
+import com.example.blog_api.dto.CommentCreateDTO;
+import com.example.blog_api.dto.CommentDTO;
+import com.example.blog_api.dto.CommentUpdateDTO;
+import com.example.blog_api.service.impl.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+
+    // create a comment by user to postId
+    @PostMapping
+    public ResponseEntity<ApiResponse<CommentDTO>> create(@RequestBody CommentCreateDTO dto) {
+
+        CommentDTO saved = commentService.create(dto);
+
+        return new ResponseEntity<>(
+                new ApiResponse<>(201, "Comment added successfully", saved),
+                HttpStatus.CREATED
+        );
+    }
+
+    //  update comment by commentId - user
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<CommentDTO>> update(
+            @PathVariable Long id,
+            @RequestBody CommentUpdateDTO dto) {
+
+        CommentDTO updated = commentService.update(id, dto);
+
+        return ResponseEntity.ok(
+                new ApiResponse<>(200, "Comment updated successfully", updated)
+        );
+    }
+
+    // delete comment by commentId - user
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<String>> delete(@PathVariable Long id) {
+
+        commentService.delete(id);
+
+        return ResponseEntity.ok(
+                new ApiResponse<>(200, "Comment deleted successfully", "OK")
+        );
+    }
+
+    // get comment by CommentId
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<CommentDTO>> getById(@PathVariable Long id) {
+
+        CommentDTO dto = commentService.getById(id);
+
+        return ResponseEntity.ok(
+                new ApiResponse<>(200, "Comment fetched successfully", dto)
+        );
+    }
+
+    // get list of comments by postId
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<ApiResponse<List<CommentDTO>>> getByPost(@PathVariable Long postId) {
+
+        List<CommentDTO> list = commentService.getAllCommentsForPost(postId);
+
+        return ResponseEntity.ok(
+                new ApiResponse<>(200, "Comments fetched successfully", list)
+        );
+    }
+
+
+}
